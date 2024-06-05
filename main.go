e0570b11498932fd750d311bde8959555c56def3
Mohammad Rahimi <moyi.pary@gmail.com>
2024-06-05 13:51:16 +0330

    first init of TUI practice

---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	choices  []string
+	cursor   int
+	selected map[int]any
+}
+
+func initialModel() model {
+	return model{
+		choices: []string{
+			"Buy carrots",
+			"Buy orange",
+			"Buy kohlrabi",
+			"Buy milk",
+			"Buy meat",
+			"Buy bread",
+			"Buy apple",
+			"Buy cheese",
+			"Buy potato",
+		},
+		cursor:   0,
+		selected: make(map[int]any),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key and the space bar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+
+		case "alt+c":
+			m.cursor = 0
+			for i, _ := range m.selected {
+				delete(m.selected, i)
+			}
+
+		case "alt+a":
+			m.cursor = len(m.choices) - 1
+			for i := range m.choices {
+				m.selected[i] = struct{}{}
+			}
+		}
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "What should we buy at market?\n\n"
+
+	for i, choice := range m.choices {
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	s += "\nPress q to quit."
+
+	return s
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an err: %v", err)
+		os.Exit(1)
+	}
+}
